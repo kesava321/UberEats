6934ebddbc092e956f20d79bec6b9d2fd817a79f
Kesava Uthaya-Kumaran <kesava@live.co.uk>
2020-03-24 18:42:05 +0000

    Added toolbar with menu button

--- app/src/main/java/com/example/ubereats/CustomerMainActivity.java
+++ app/src/main/java/com/example/ubereats/CustomerMainActivity.java
@@ -3,10 +3,15 @@ package com.example.ubereats;
 import android.os.Bundle;
 import android.view.MenuItem;
 
+
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.material.navigation.NavigationView;
+
 
 public class CustomerMainActivity extends AppCompatActivity {
 
@@ -16,6 +21,12 @@ public class CustomerMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
 
         mDrawerLayout = findViewById(R.id.drawer_layout);
 
@@ -36,4 +47,14 @@ public class CustomerMainActivity extends AppCompatActivity {
                 }
     });
 }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
