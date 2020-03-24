4b0241086452e6af61fedaea9f76ea41fe243bdd
Kesava Uthaya-Kumaran <kesava@live.co.uk>
2020-03-24 17:16:46 +0000

    Implemented functionality for drawer_view and fixed checkableBehavior error.

--- app/src/main/java/com/example/ubereats/CustomerMainActivity.java
+++ app/src/main/java/com/example/ubereats/CustomerMainActivity.java
@@ -1,13 +1,39 @@
 package com.example.ubereats;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class CustomerMainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer);
-    }
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        //set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        //close drawer when item is tapped
+                        mDrawerLayout.closeDrawers();
+
+                        //Add code here to update the UI based on the item selected
+                        //For example, swap UI fragments here
+
+                        return true;
+                }
+    });
+}
 }
